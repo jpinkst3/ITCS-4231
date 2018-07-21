098fc27ec47981007fc6d77d5934a1e25b2e7120
jpinkst3 <jpinkst3@uncc.edu>
2018-07-20 21:18:37 -0700

    EXPLODING BOXES!
    Added an exploding box for the vertical platforming level! Its under room 4 -> BoomBox

---
+++ Assets/Scripts/BoomBox.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BoomBox : MonoBehaviour {
+
+    public float delay = 10f;
+    float countdown;
+    bool hasBoom = false;
+    public GameObject Explosion;
+
+	void Start () {
+        countdown = delay;
+	}
+	
+	void Update () {
+        countdown -= Time.deltaTime;
+        if(countdown <= 0f && !hasBoom)
+        {
+            Explode();
+            hasBoom = true;
+        }
+	}
+
+    void Explode()
+    {
+        Debug.Log("BOOM!");
+        Instantiate(Explosion, transform.position, transform.rotation);
+        Destroy(gameObject);
+    }
+}
