17e855d3ec49d053cfb18aa47ee7a075d33db660
jpinkst3 <jpinkst3@uncc.edu>
2018-07-24 11:11:14 -0700

    General Tweeks
    Did stuff

---
+++ Assets/Scripts/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    private float nextSpawnTime;
+    [SerializeField] private GameObject Boom_Box;
+    [SerializeField] private float delay = 12;
+
+    private void Update()
+    {
+        if(ShouldSpawn())
+        {
+            Spawn();
+        }
+    }
+
+    private void Spawn()
+    {
+        nextSpawnTime = Time.time + delay;
+        Instantiate(Boom_Box, transform.position, transform.rotation);
+    }
+
+    private bool ShouldSpawn()
+    {
+        return Time.time >= nextSpawnTime;
+    }
+}
