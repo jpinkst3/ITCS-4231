fcbf8000a010900ac5295f8dc22cd759e35d4094
jpinkst3 <jpinkst3@uncc.edu>
2018-07-30 21:15:08 -0700

    Countdown
    Added a countdown. Doesnt quite work on the boxes though.

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using UnityEngine.UI;
+using UnityEngine;
+
+public class Countdown : MonoBehaviour {
+    Image fillImg;
+    float timeAmt = 5;
+    float time;
+	// Use this for initialization
+	void Start ()
+    {
+        fillImg = this.GetComponent<Image>();
+        time = timeAmt;
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+		if(time > 0)
+        {
+            time -= Time.deltaTime;
+            fillImg.fillAmount = time / timeAmt;
+        }
+	}
+}
